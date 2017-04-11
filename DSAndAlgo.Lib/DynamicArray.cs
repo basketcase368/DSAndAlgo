76aea66c8f28e192c88cd9e6c095d43ca43ff55d
Humberto <hsainz@innovasi.com>
2017-04-11 14:23:54 -0700

    Created DynamicArray example.

---
+++ DSAndAlgo.Lib/DynamicArray.cs
@@ -0,0 +1,43 @@
+﻿using System;
+namespace DSAndAlgo.Lib
+{
+    public class DynamicArray<T>
+    {
+        private int _size;
+        private Object[] _array;
+
+        public DynamicArray()
+        {
+            _size = 0;
+            _array = new Object[1];
+        }
+
+        public int GetSize()
+        {
+            return _array.Length;
+        }
+
+        public Object Get(int index)
+        {
+            return (T)_array[index];
+        }
+
+        public void Add(T item)
+        {
+            checkForFit(_size + 1);
+            _array[_size++] = item;
+        }
+
+        private void checkForFit(int minReqCapacity)
+        {
+            int oldCapacity = GetSize();
+            if(minReqCapacity > oldCapacity)
+            {
+                int newCapacity = oldCapacity * 2;
+                var temp = new Object[newCapacity];
+                Array.Copy(_array, temp, _array.Length);
+                _array = temp;
+            }
+        }
+    }
+}
--- DSAndAlgoMain/Program.cs
+++ DSAndAlgoMain/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using DSAndAlgo.Lib;
 
 namespace DSAndAlgoMain
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var da = new DynamicArray<int>();
+            Console.WriteLine("Current Size: {0}", da.GetSize());
+            da.Add(1);
+            Console.WriteLine("Current Size: {0}", da.GetSize());
+            da.Add(2);
+            Console.WriteLine("Current Size: {0}", da.GetSize());
+            da.Add(3);
+            Console.WriteLine("Current Size: {0}", da.GetSize());
+            da.Add(4);
+            Console.WriteLine("Current Size: {0}", da.GetSize());
+            da.Add(5);
+
+            Console.ReadLine();
         }
     }
 }
